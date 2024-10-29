e73922eabdd37579587a97aba61c544c0058eb30
LikoKuridze <lkuridze@credo.ge>
2024-10-29 23:05:32 +0400

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string[] students = new string[3];
+
+for (int i = 0; i < students.Length; i++)
+{
+    Console.Write("Enter the name of student: ");
+    students[i] = Console.ReadLine();
+}
+
+
+Console.WriteLine("All students:");
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine(students[i]);
+}
